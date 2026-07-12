3411256792a83c065f560879553551782bf192bb
Sakabrata <141290411+Sakabrata@users.noreply.github.com>
2026-07-12 15:02:35 +0000

    Fix CSP secret leak

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://api.sandbox.midtrans.com https://pay.google.com https://gwk.gopayapi.com https://www.googletagmanager.com;
-              connect-src 'self' https://*.supabase.co wss://*.supabase.co https://asicyirapausnyphsqov.supabase.co wss://asicyirapausnyphsqov.supabase.co https://app.sandbox.midtrans.com https://api.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://pay.google.com https://www.google-analytics.com;
+              connect-src 'self' https://*.supabase.co wss://*.supabase.co ${process.env.NEXT_PUBLIC_SUPABASE_URL} wss://${process.env.NEXT_PUBLIC_SUPABASE_URL?.replace("https://", "")} https://app.sandbox.midtrans.com https://api.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://pay.google.com https://www.google-analytics.com;
               frame-src 'self' https://app.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://pay.google.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               font-src 'self' data: https://fonts.gstatic.com;
